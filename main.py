f23ddfd55d08dfd0cfa89aad5961e2f8cda1b666
Claudio <leclaudiohenrique@gmail.com>
2022-12-04 22:02:29 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+# Press the green button in the gutter to run the script.
+authorizer = DummyAuthorizer()
+authorizer.add_user("user", "1234", 'C:/Users/chcbe/Desktop/Puc Minas/Projetos/Java/redes_guia_3/src/main/java/com/redes/redes/Logs')
+authorizer.add_anonymous('C:/Users/chcbe/Desktop/Puc Minas/Projetos/Java/redes_guia_3/src/main/java/com/redes/redes/Logs')
+handler = FTPHandler
+handler.authorizer = authorizer
+server = FTPServer(("127.0.0.1", 21), handler)
+server.serve_forever()
