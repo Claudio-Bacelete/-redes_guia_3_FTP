955995e993d01e753ffba12d7c105927245714a4
Claudio <leclaudiohenrique@gmail.com>
2022-12-04 22:43:16 -0300

    Mudar usuário

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyftpdlib.servers import FTPServer
 
 # Press the green button in the gutter to run the script.
 authorizer = DummyAuthorizer()
-authorizer.add_user("user", "1234", 'C:/Users/chcbe/Desktop/Puc Minas/Projetos/Java/redes_guia_3/src/main/java/com/redes/redes/Logs')
+authorizer.add_user("teste", "1234", 'C:/Users/chcbe/Desktop/Puc Minas/Projetos/Java/redes_guia_3/src/main/java/com/redes/redes/Logs')
 authorizer.add_anonymous('C:/Users/chcbe/Desktop/Puc Minas/Projetos/Java/redes_guia_3/src/main/java/com/redes/redes/Logs')
 handler = FTPHandler
 handler.authorizer = authorizer
